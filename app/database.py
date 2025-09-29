616038fdf0b787ec58b7ea6234575cc796ecdd5f
Allan Mwenja <140613367+mahianyuallan@users.noreply.github.com>
2025-09-29 17:46:11 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Replace with your actual DB credentials
+
 DATABASE_URL = "postgresql://postgres:All%40n108%23123@localhost:5432/notesdb"
 
 engine = create_engine(DATABASE_URL)
